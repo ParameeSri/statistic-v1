70de71163d71be857e3f43fff84af7058e9a7447
paramee <Paramee.Srira@silkspan.local>
2018-12-03 17:30:22 +0700

    commit init

---
+++ pages/statistic/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <section class="container">
+      xxx
+    </section>
+  </template>
+  
+  <script>
+  import Logo from '~/components/Logo.vue'
+  
+  export default {
+    components: {
+      Logo
+    }
+  }
+  </script>
